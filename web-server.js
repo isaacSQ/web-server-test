c3ba0e0015103e52263b0278e689ed8a623975a5
Isaac Hargreaves <isaachargreaves.sq@gmail.com>
2024-09-03 11:19:28 +0100

    alters test to listen for host

--- web-server.js
+++ web-server.js
@@ -1,11 +1,11 @@
 const net = require('net');
 const dgram = require('dgram');
 
-const TCP_PORT = 22023;
-const UDP_PORT = 41064;
+//const TCP_PORT = 22023;
+const UDP_PORT = 22023 //41064;
 const FORWARDING_IP = '192.168.68.111';
 
-    const udpServer = dgram.createSocket('udp4');
+const udpServer = dgram.createSocket({type: 'udp4', reuseAddr: true});
 
     udpServer.on('listening', () => {
         const address = udpServer.address();
@@ -14,9 +14,11 @@ const FORWARDING_IP = '192.168.68.111';
     
     udpServer.on('message', (msg, rinfo) => {
         console.log(`UDP WEB Server received: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+        const response = Buffer.from('HOST')
         
-        udpServer.send(msg, 0, msg.length, UDP_PORT, FORWARDING_IP, (err) => {
-            console.log(`UDP WEB message sent to`)
+        udpServer.send(response, 0, response.length, rinfo.port, rinfo.address, (err) => {
+            console.log(`UDP WEB message sent to ${rinfo.address}`);
             if (err) console.error('UDP WEB send error:', err);
         });
     });
@@ -28,27 +30,27 @@ const FORWARDING_IP = '192.168.68.111';
     
     udpServer.bind(UDP_PORT);
     
-    const tcpServer = net.createServer((socket) => {
-        console.log('TCP client connected:', socket.remoteAddress);
+//     const tcpServer = net.createServer((socket) => {
+//         console.log('TCP client connected:', socket.remoteAddress);
         
-        socket.on('data', (data) => {
-            console.log(`TCP Server received: ${data} from ${socket.remoteAddress}:${socket.remotePort}`);
+//         socket.on('data', (data) => {
+//             console.log(`TCP Server received: ${data} from ${socket.remoteAddress}:${socket.remotePort}`);
             
-            udpServer.send(data, 0, data.length, UDP_PORT, FORWARDING_IP, (err) => {
-                console.log(`UDP WEB message sent to`)
-                if (err) console.error('UDP WEB send error:', err);
-            });
-        });
+//             udpServer.send(data, 0, data.length, UDP_PORT, FORWARDING_IP, (err) => {
+//                 console.log(`UDP WEB message sent to`)
+//                 if (err) console.error('UDP WEB send error:', err);
+//             });
+//         });
         
-    socket.on('end', () => {
-        console.log('TCP client disconnected');
-    });
+//     socket.on('end', () => {
+//         console.log('TCP client disconnected');
+//     });
 
-    socket.on('error', (err) => {
-        console.error(`TCP Server error:\n${err.stack}`);
-    });
-});
+//     socket.on('error', (err) => {
+//         console.error(`TCP Server error:\n${err.stack}`);
+//     });
+// });
 
-tcpServer.listen(TCP_PORT, () => {
-    console.log(`TCP Server listening on port ${TCP_PORT}`);
-});
+// tcpServer.listen(TCP_PORT, () => {
+//     console.log(`TCP Server listening on port ${TCP_PORT}`);
+// });
