8b52429053198c88dfa18e76b061c388de1974d8
Isaac Hargreaves <isaachargreaves.sq@gmail.com>
2024-09-10 12:03:54 +0100

    168

--- web-server.js
+++ web-server.js
@@ -25,76 +25,77 @@ let tcpClientId = {}
 
 const app = express()
 
+// app.get('/', (req, res)=>{
+//     if(req.query?.id){
 
-app.get('/', (req, res)=>{
-    if(req.query?.id){
+//         let unid = req.query.id
 
-        let unid = req.query.id
+
         
-        const pictureToServe = fs.readFileSync(
-                                appPath + "/images/picture_questions/current_picture_question.jpg"
-                            )
-        
-        
-        res.setHeader('Content-Type', 'image/jpeg')
-        
-        res.end(pictureToServe, "binary")
-        
-        res.on("finish", function () {
-            console.log('Image Served')
-            if(unid !== "ios2921I8C5593A46"){
-                process.send({ command: "server_picture_served", unid: unid })
-            }
-            res.destroy()
-        })
+//         const pictureToServe = fs.readFileSync(
+//                                 appPath + "/images/picture_questions/current_picture_question.jpg"
+//                             )
         
         
+//         res.setHeader('Content-Type', 'image/jpeg')
+        
+//         res.end(pictureToServe, "binary")
+        
+//         res.on("finish", function () {
+//             console.log('Image Served')
+//             if(unid !== "ios2921I8C5593A46"){
+//                 process.send({ command: "server_picture_served", unid: unid })
+//             }
+//             res.destroy()
+//         })
         
         
-        }else{
-        
-            res.setHeader('Content-Type', 'application/json')
-            res.json({error:'no_params'})
         
         
-        }
-})
+//         }else{
+        
+//             res.setHeader('Content-Type', 'application/json')
+//             res.json({error:'no_params'})
+        
+        
+//         }
+// })
 
-app.get('/get_round_pictures', (req,res) => {
+// app.get('/get_round_pictures', (req,res) => {
 
-	if(req.query?.id){
+// 	if(req.query?.id){
 
-	let unid = req.query.id
+// 	let unid = req.query.id
 
-	console.log("- - - - - - - - - - - - -------- PICTURES ZIP ROUTE ---------------------------- ")
+// 	console.log("- - - - - - - - - - - - -------- PICTURES ZIP ROUTE ---------------------------- ")
 
-		if(unid ==  "ios2921I8C5593A46"){
+// 		if(unid ==  "ios2921I8C5593A46"){
 
-			console.log(unid,"- - - - - - - - - - - - -------- 404 PICTURES ZIP ROUTE 404 ---------------------------- ")
+// 			console.log(unid,"- - - - - - - - - - - - -------- 404 PICTURES ZIP ROUTE 404 ---------------------------- ")
 
-			res.sendStatus(500)
+// 			res.sendStatus(500)
 
-		}else{
+// 		}else{
 
-			res.setHeader('Content-Type', 'application/zip')
+// 			res.setHeader('Content-Type', 'application/zip')
 
-			fs.readdir(appPath + "/images/roundpics", (err, files) => {
-				res.end(
-					fs.readFileSync(appPath + "/images/roundpics/" + files[0]),
-					"binary"
-				)
-			})
+// 			fs.readdir(appPath + "/images/roundpics", (err, files) => {
+// 				res.end(
+// 					fs.readFileSync(appPath + "/images/roundpics/" + files[0]),
+// 					"binary"
+// 				)
+// 			})
 
-		}
+// 		}
 
-	}else{
+// 	}else{
 
-		res.setHeader('Content-Type', 'application/json')
-		res.json({error:'no_params'})
+// 		res.setHeader('Content-Type', 'application/json')
+// 		res.json({error:'no_params'})
 
-	}
+// 	}
 
-})
+// })
 
 /* CLIP LIST ROUTE */
 
@@ -102,8 +103,8 @@ app.get('/clips', (req,res) => {
 
 
 	if(locallyStoredBuzzerClips == null){
-		var dataFilePath = path.join(docsPath, "._sq_imported/setdbv5.json")
-		var data = JSON.parse(fs.readFileSync(dataFilePath, "utf-8"))
+		//var dataFilePath = path.join(docsPath, "._sq_imported/setdbv5.json")
+		var data = `[{"file":"A lot more to ogres than you think.mp3"},{"file":"Ah go on.mp3"},{"file":"Ahaaa.mp3"},{"file":"Alrighty then.mp3"},{"file":"And in the morning I'm making waffles.mp3"},{"file":"Are you having a laugh.mp3"},{"file":"Are you talking to me.mp3"},{"file":"Bazinga.mp3"},{"file":"Booyakasha.mp3"},{"file":"Born to be a popstar.mp3"},{"file":"Circle him here back of the net.mp3"},{"file":"Day four.mp3"},{"file":"Ding dong.mp3"},{"file":"Do you expect me to talk.mp3"},{"file":"Eat my waffle.mp3"},{"file":"Ere so shut up.mp3"},{"file":"Gavalaaaa.mp3"},{"file":"Goal of the month competition.mp3"},{"file":"Great Scott.mp3"},{"file":"Ha haaa.mp3"},{"file":"Hahahahaaaa Hahahahahaaaaa.mp3"},{"file":"Happy with the win.mp3"},{"file":"Hey Kids hoohoohahahaha.mp3"},{"file":"High five.mp3"},{"file":"How I got these scars.mp3"},{"file":"How you doin.mp3"},{"file":"I beg pardon.mp3"},{"file":"I dont beleive it.mp3"},{"file":"I have a cunning plan.mp3"},{"file":"I just dont see how I can make any money.mp3"},{"file":"I just need a drink.mp3"},{"file":"I like you very much.mp3"},{"file":"I'm ready.mp3"},{"file":"I've pierced my foot on a spyke.mp3"},{"file":"In Alaska hunting wolverines.mp3"},{"file":"It wasn't My Fault it was George.mp3"},{"file":"It's the wrong trousers.mp3"},{"file":"Just an oedinary lad.mp3"},{"file":"Just calm down.mp3"},{"file":"Know what I mean.mp3"},{"file":"Lamb mint sauce bang.mp3"},{"file":"Laugh.mp3"},{"file":"May the force be with you.mp3"},{"file":"Means family.mp3"},{"file":"Merry Christmas you filthy animals.mp3"},{"file":"My Precious.mp3"},{"file":"Nice to see you.mp3"},{"file":"No likey no litey.mp3"},{"file":"Oh David.mp3"},{"file":"Oh Philip.mp3"},{"file":"Oh my god.mp3"},{"file":"Oh you dirty old man.mp3"},{"file":"One million dollars.mp3"},{"file":"One point 21 jiggawatts.mp3"},{"file":"Oooh friend   friend.mp3"},{"file":"Ozzy gets his M and Ms.mp3"},{"file":"Pete's eying up my Buhnas.mp3"},{"file":"Peter come on.mp3"},{"file":"Potatoes.mp3"},{"file":"Right lets play.mp3"},{"file":"Shaken not stirred.mp3"},{"file":"Shut your mouth you'll do nuttin.mp3"},{"file":"Six thousand pounds.mp3"},{"file":"Some of the housemates.mp3"},{"file":"Stop it Zippy.mp3"},{"file":"Talk to me.mp3"},{"file":"Terrific player.mp3"},{"file":"Think McFly.mp3"},{"file":"Tina you fat lard.mp3"},{"file":"To not pay attention.mp3"},{"file":"Very very happy with the win.mp3"},{"file":"We're gonna make America great again.mp3"},{"file":"We've gone on holiday by mistake.mp3"},{"file":"Why is the rum always gone.mp3"},{"file":"X O X O.mp3"},{"file":"Yeah I know I want that one.mp3"},{"file":"Yeez have to be joking.mp3"},{"file":"Yes I can hear you Clem Fandango.mp3"},{"file":"Zoinks Lets get out of here.mp3"}]`
 
 
 		locallyStoredBuzzerClips = data["buzzer_clips"].map(function (clip) {
@@ -128,251 +129,251 @@ app.get('/clips', (req,res) => {
 
 /* CLIP USED ROUTE */
 
-app.get('/clips_used', (req,res) => {
+// app.get('/clips_used', (req,res) => {
 
-	res.setHeader('Content-Type', 'application/json')
+// 	res.setHeader('Content-Type', 'application/json')
 
-	if(req.query?.unid){
+// 	if(req.query?.unid){
 
-	const unid = req.query.unid
+// 	const unid = req.query.unid
 
-	var used = []
+// 	var used = []
 
-	var selected = -1
+// 	var selected = -1
 
-	//discover if device connecting has already selected a sound
+// 	//discover if device connecting has already selected a sound
 
-	if (allocatedClipsArray.length > 0) {
-		var item;
-		item = allocatedClipsArray.find(function (clip) {
-			return unid === clip.usedby
-		});
+// 	if (allocatedClipsArray.length > 0) {
+// 		var item;
+// 		item = allocatedClipsArray.find(function (clip) {
+// 			return unid === clip.usedby
+// 		});
 
-		if (item !== undefined) selected = item.index;
+// 		if (item !== undefined) selected = item.index;
 
-		used = allocatedClipsArray
-			.filter(function (clip) {
-				return clip.index !== selected
-			})
-			.map(function (clip) {
-				return clip.index
-			});
-	}
+// 		used = allocatedClipsArray
+// 			.filter(function (clip) {
+// 				return clip.index !== selected
+// 			})
+// 			.map(function (clip) {
+// 				return clip.index
+// 			});
+// 	}
 
-	var resstr = '{"used_clips": "' + used.toString() + '","selected_clip": "' + selected + '"}'
+// 	var resstr = '{"used_clips": "' + used.toString() + '","selected_clip": "' + selected + '"}'
 
-	console.log("-- SERVED CLIP USED /clips_used ---",resstr)
+// 	console.log("-- SERVED CLIP USED /clips_used ---",resstr)
 
-	res.end(resstr)
+// 	res.end(resstr)
 
-	}else{
+// 	}else{
 
-	res.json({error:'no_params'})
-	}
+// 	res.json({error:'no_params'})
+// 	}
 
-})
+// })
 
 
 
 
 
-app.get('/advert-*', (req,res) => {
-	//ADVERTS
+// app.get('/advert-*', (req,res) => {
+// 	//ADVERTS
 
-	console.log(req.url)
+// 	console.log(req.url)
 
-	var filename = req.url.substr(1)
+// 	var filename = req.url.substr(1)
 
-	var pth = docsPath + "/._sq_imported/_handset_slides/";
+// 	var pth = docsPath + "/._sq_imported/_handset_slides/";
 
-	fs.readFile(pth + filename, function (err, file) {
-		//in case
-		if (err) file = fs.readFileSync(pth + "-sq-advert-1.jpg")
+// 	fs.readFile(pth + filename, function (err, file) {
+// 		//in case
+// 		if (err) file = fs.readFileSync(pth + "-sq-advert-1.jpg")
 
-		res.setHeader('Content-Type', 'image/jpeg')
+// 		res.setHeader('Content-Type', 'image/jpeg')
 
-		res.end(file, "binary")
+// 		res.end(file, "binary")
 
-		res.on('finish', function() {
-					console.log('Image Served')
-		      process.send({'command':'server_advert_served',ip:res.remoteAddress})
-		   })
+// 		res.on('finish', function() {
+// 					console.log('Image Served')
+// 		      process.send({'command':'server_advert_served',ip:res.remoteAddress})
+// 		   })
 
-	})
+// 	})
 
 
-})
+// })
 
-/* SCOREBOARD ROUTE */
+// /* SCOREBOARD ROUTE */
 
-app.get('/get_scoreboard', (req,res) => {
+// app.get('/get_scoreboard', (req,res) => {
 
-	res.setHeader('Content-Type', 'application/json')
-	//to be removed when 5.5.6 is minimum for host V5
-	let isV5 = req.query?.v5 ?? false
-	//  ----  //
-	if(req.query?.unid){
+// 	res.setHeader('Content-Type', 'application/json')
+// 	//to be removed when 5.5.6 is minimum for host V5
+// 	let isV5 = req.query?.v5 ?? false
+// 	//  ----  //
+// 	if(req.query?.unid){
 
-		const unid = req.query.unid
+// 		const unid = req.query.unid
 
-			const scoreboardArrWithHighlight = scoreboardArr.map(item => {
-				// ---
-			const name = isV5 ? item.name : item.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-				// ---
-			const owned = unid === item.id;
-			  return {
-			    pos: item.pos,
-			    name,
-			    score: item.score,
-				tab_colour: item.tab_colour,
-			    owned,
-			  };
-			});
+// 			const scoreboardArrWithHighlight = scoreboardArr.map(item => {
+// 				// ---
+// 			const name = isV5 ? item.name : item.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+// 				// ---
+// 			const owned = unid === item.id;
+// 			  return {
+// 			    pos: item.pos,
+// 			    name,
+// 			    score: item.score,
+// 				tab_colour: item.tab_colour,
+// 			    owned,
+// 			  };
+// 			});
 
 
-		res.json(scoreboardArrWithHighlight)
-		res.destroy()
+// 		res.json(scoreboardArrWithHighlight)
+// 		res.destroy()
 
-		}else{
+// 		}else{
 
-		res.json({error:'no_params'})
+// 		res.json({error:'no_params'})
 
-		}
+// 		}
 
-})
+// })
 
-/* LANGUAGE ROUTE */
-app.get('/get_i18n', (req,res) => {
+// /* LANGUAGE ROUTE */
+// app.get('/get_i18n', (req,res) => {
 
 
-	if(req.query?.hash){
-		process.send({ command: "lang_check_update", hash: req.query.hash })
-	} else
-		process.send({ command: "lang_check_update", hash: "sillysausages" })
+// 	if(req.query?.hash){
+// 		process.send({ command: "lang_check_update", hash: req.query.hash })
+// 	} else
+// 		process.send({ command: "lang_check_update", hash: "sillysausages" })
 
 
-	//waiting 2000 for appLanguageJson update
-	setTimeout(function () {
-		res.setHeader('Content-Type', 'application/json')
-	  res.end(appLanguageJson)
-		res.destroy()
-	}, 2000)
+// 	//waiting 2000 for appLanguageJson update
+// 	setTimeout(function () {
+// 		res.setHeader('Content-Type', 'application/json')
+// 	  res.end(appLanguageJson)
+// 		res.destroy()
+// 	}, 2000)
 
-})
+// })
 
 
 
-/* PROFILE PICTURES ROUTE */
+// /* PROFILE PICTURES ROUTE */
 
-app.get('/winning_team_pic', (req,res) => {
+// app.get('/winning_team_pic', (req,res) => {
 
-	res.setHeader('Content-Type', 'image/jpeg')
+// 	res.setHeader('Content-Type', 'image/jpeg')
 
-	res.end(
-		fs.readFileSync(docsPath + "Player Images/current_winning_team.jpg"),
-		"binary"
-	)
+// 	res.end(
+// 		fs.readFileSync(docsPath + "Player Images/current_winning_team.jpg"),
+// 		"binary"
+// 	)
 
-})
+// })
 
-app.get('/rotated_team_pic', (req,res) => {
+// app.get('/rotated_team_pic', (req,res) => {
 
-	if(req.query?.picId){
-		const picId = req.query.picId
-		res.setHeader('Content-Type', 'image/jpeg')
-		res.end(
-			fs.readFileSync(docsPath + `Player Images/${picId}.jpg`),
-			"binary"
-		)
-	} else{
-		res.json({error: 'no_params'})
-	}
+// 	if(req.query?.picId){
+// 		const picId = req.query.picId
+// 		res.setHeader('Content-Type', 'image/jpeg')
+// 		res.end(
+// 			fs.readFileSync(docsPath + `Player Images/${picId}.jpg`),
+// 			"binary"
+// 		)
+// 	} else{
+// 		res.json({error: 'no_params'})
+// 	}
 
 
 
-})
+// })
 
-/* BINGO CARD ROUTE */
+// /* BINGO CARD ROUTE */
 
-app.get('/get_bingo_card', (req,res) => {
+// app.get('/get_bingo_card', (req,res) => {
 
-	//console.log("-------------- - - - - - - - - BINGO CARD REQ... ",req.url, req.query)
+// 	//console.log("-------------- - - - - - - - - BINGO CARD REQ... ",req.url, req.query)
 
-	if(req.query?.unid){
+// 	if(req.query?.unid){
 
-	const unid = req.query.unid
+// 	const unid = req.query.unid
 
-	//console.log("BINGO CARD",unid,bingoCardsObj[unid])
+// 	//console.log("BINGO CARD",unid,bingoCardsObj[unid])
 
-	var used = []
+// 	var used = []
 
-	var selected = -1
+// 	var selected = -1
 
-	res.setHeader('Content-Type', 'application/json')
+// 	res.setHeader('Content-Type', 'application/json')
 
-	res.json(bingoCardsObj[unid])
-	//res.destroy()
+// 	res.json(bingoCardsObj[unid])
+// 	//res.destroy()
 
-	}else{
-		res.json({error:'no_params'})
-	}
+// 	}else{
+// 		res.json({error:'no_params'})
+// 	}
 
-})
+// })
 
-app.get('/get_wheel_list', (req,res) => {
+// app.get('/get_wheel_list', (req,res) => {
 
-	//console.log("-------------- - - - - - - - - BINGO CARD REQ... ",req.url, req.query)
+// 	//console.log("-------------- - - - - - - - - BINGO CARD REQ... ",req.url, req.query)
 
 
-	const unid = req.query.unid
+// 	const unid = req.query.unid
 
-	//console.log("BINGO CARD",unid,bingoCardsObj[unid])
+// 	//console.log("BINGO CARD",unid,bingoCardsObj[unid])
 
-	res.setHeader('Content-Type', 'application/json')
+// 	res.setHeader('Content-Type', 'application/json')
 
-	res.json(wheelList)
-	//res.destroy()
+// 	res.json(wheelList)
+// 	//res.destroy()
 
 
-})
+// })
 
 
-/* WHEEL ROUTE */
+// /* WHEEL ROUTE */
 
-app.get('/get_wheel', (req,res) => {
+// app.get('/get_wheel', (req,res) => {
 
 
 
-	res.sendFile(publicPath + `/wheel.html`)
+// 	res.sendFile(publicPath + `/wheel.html`)
 
-})
-app.get('/assets/FontManifest.json', (req,res) => {
-	res.json([])
-})
+// })
+// app.get('/assets/FontManifest.json', (req,res) => {
+// 	res.json([])
+// })
 
-/* TEST ROUTE */
+// /* TEST ROUTE */
 
-app.get('/test', (req,res) => {
+// app.get('/test', (req,res) => {
 
-	res.setHeader('Content-Type', 'application/json')
+// 	res.setHeader('Content-Type', 'application/json')
 
-    res.json({
-		SpeedQuizzing: true,
-		media_server: true,
-		req: req.url,
-		daftness: 19,
-	})
+//     res.json({
+// 		SpeedQuizzing: true,
+// 		media_server: true,
+// 		req: req.url,
+// 		daftness: 19,
+// 	})
 
-})
+// })
 
 
-app.get('/health-check', (req,res) => {
+// app.get('/health-check', (req,res) => {
 
-    res.setHeader('Content-Type', 'application/json')
+//     res.setHeader('Content-Type', 'application/json')
 
-    res.json({"status":"healthy"})
+//     res.json({"status":"healthy"})
 
-})
+// })
 
 app.listen(2024, ()=>{
     console.log('WEB Server listening on port 2024');
