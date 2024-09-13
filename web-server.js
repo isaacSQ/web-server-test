4a1ea9b3125d5cac1f0c6f41c64df936cde9b4ad
Isaac Hargreaves <isaachargreaves.sq@gmail.com>
2024-09-13 15:45:31 +0100

    1

--- web-server.js
+++ web-server.js
@@ -123,7 +123,7 @@ app.post('/process_update', (req, res)=>{
 })
 
 app.post('/advert-*', (req, res)=>{
-    console.log("HERE HERE HERE HERE", req)
+    console.log("HERE HERE HERE HERE", req.body)
     const filename = req.url.slice(1)
     console.log("ADVERT", filename)
     advertsObject[filename] = req.body
