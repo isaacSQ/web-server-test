dd21c642ec40d2d343096cb37ca861503c887e7e
Isaac Hargreaves <isaachargreaves.sq@gmail.com>
2024-09-16 14:20:42 +0100

    1

--- web-server.js
+++ web-server.js
@@ -602,7 +602,7 @@ function forwardTcpToClient(buffer) {
 
 let hostDataContent = ""
 function forwardTcpToHost(buffer, socket) {
-    console.log("🚀 ~ forwardTcpToHost ~ buffer:", buffer.toString())
+    console.log("🚀 ~ forwardTcpToHost ~ buffer:", buffer.toString().slice(0,50), buffer.toString().slice(buffer.length - 50))
     let data = hostDataContent + buffer
     console.log("data:", data.slice(0,20),"...", data.slice(data.length - 100))
 
