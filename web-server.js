2d7ea1e6e961c3562402bcae4e7dafa4548a407c
Isaac Hargreaves <isaachargreaves.sq@gmail.com>
2024-09-18 13:20:28 +0100

    1

--- web-server.js
+++ web-server.js
@@ -38,7 +38,7 @@ let processObject = {
 
 const app = express();
 
-app.use(express.json({limit: '50mb'}));
+app.use(express.json());
 app.use(express.urlencoded({limit: '50mb'}));
 
 app.get('/', (req, res)=>{
